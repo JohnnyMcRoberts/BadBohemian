04b86ec9e0cf98da7a24a672b83e40169066d245
Johnny McRoberts <johnathanmcroberts@gmail.com>
2018-03-10 16:33:04 +0000

    Mess in test to sort out later - 1 more added

---
+++ MongoBooks2/BooksLiveCharts/ViewModels/PieCharts/CurrentPagesReadByCountryPieChartViewModel.cs
@@ -0,0 +1,65 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CurrentPagesReadByCountryPieChartViewModel.cs" company="N/A">
+//   2016
+// </copyright>
+// <summary>
+//   The base pie-chart view model.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace BooksLiveCharts.ViewModels.PieCharts
+{
+    using System.Collections.Generic;
+    using System.Windows.Media;
+    using BooksCore.Utilities;
+    using BooksUtilities.Colors;
+    using LiveCharts;
+    using LiveCharts.Definitions.Series;
+
+    /// <summary>
+    /// The pages by country pie chart view model class.
+    /// </summary>
+    public sealed class CurrentPagesReadByCountryPieChartViewModel : BasePieChartViewModel
+    {
+        /// <summary>
+        /// Sets up the pie chart series.
+        /// </summary>
+        protected override void SetupSeries()
+        {
+            // If no books return the default.
+            if (BooksReadProvider == null)
+            {
+                base.SetupSeries();
+                return;
+            }
+
+            // Get the sorted books.
+            List<KeyValuePair<string, int>> sortedCountryTotals =
+                BookTotalsUtilities.SortedSortedPagesReadByCountryTotals(BooksReadProvider);
+
+            // Set up the series per county.
+            Series = new SeriesCollection();
+            List<ISeriesView> seriesViews = new List<ISeriesView>();
+            List<Color> colors = ColorUtilities.SetupStandardColourSet();
+            for (int i = 0; i < sortedCountryTotals.Count; i++)
+            {
+                KeyValuePair<string, int> countryTotal = sortedCountryTotals[i];
+                Color color = colors[i % colors.Count];
+                seriesViews.Add(CreatePieSeries(countryTotal.Key, countryTotal.Value, color));
+            }
+
+            Series.AddRange(seriesViews);
+            SeriesCollection = Series;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CurrentBooksReadByCountryPieChartViewModel"/> class.
+        /// </summary>
+        public CurrentPagesReadByCountryPieChartViewModel()
+        {
+            Title = "Current Books Read by Country";
+            PointLabel = chartPoint => $"{chartPoint.Y:G3}";
+            LegendLocation = LegendLocation.Bottom;
+            SetupSeries();
+        }
+    }
+}
