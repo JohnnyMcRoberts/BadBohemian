f046d27798ef8cc80f08197619ad581811d8bf92
Johnathan McRoberts <jmcroberts@actify.com>
2018-04-16 13:40:39 +0100

    create a utility library for the testers

---
+++ MongoBooks2/BooksImportExport/Exporters/ExportType.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ExportType.cs" company="N/A">
+//   2016-2020
+// </copyright>
+// <summary>
+//   The export types enum.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace BooksImportExport.Exporters
+{
+    using BooksImportExport.Utilities;
+
+    /// <summary>
+    /// Type of export.
+    /// </summary>
+    public enum ExportType
+    {
+        [ImportExportType(Title = "Books to csv", GeneratorClass = typeof(BooksToCsvFileExport))]
+        BooksToCsv,
+
+    }
+}
---
+++ MongoBooks2/BooksImportExport/Utilities/ImportExportTypeAttribute.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ChartTypeAttribute.cs" company="N/A">
+//   2016
+// </copyright>
+// <summary>
+//   The import export type attribute.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace BooksImportExport.Utilities
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Field)]
+    public class ImportExportTypeAttribute : Attribute
+    {
+        /// <summary>
+        /// Gets or sets the title.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the generator class.
+        /// </summary>
+        public Type GeneratorClass { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ImportExportTypeAttribute"/> class.
+        /// </summary>
+        public ImportExportTypeAttribute()
+        {
+            Title = string.Empty;
+        }
+    }
+}
