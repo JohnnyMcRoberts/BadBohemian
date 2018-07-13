4d59eb8539d524b3c4a5a5107fe65d9bfa07fab8
Johnathan McRoberts <jmcroberts@actify.com>
2018-07-13 15:55:02 +0100

    crazy mc crazy but that looks pretty good.....

---
+++ MongoBooks2/JavaScript/Node/AngularCLI/my-project/src/app/books/BookData.ts
@@ -0,0 +1,18 @@
+﻿export class BookData {
+  "_id": string;
+  "dateString": string;
+  "date": Date;
+  "author": string;
+  "title": string;
+  "pages": number;
+  "note": string;
+  "nationality": string;
+  "originalLanguage": string;
+  "image_url": string;
+  "tags": Array<string>;
+  "Book": string;
+  "Comic": string;
+  "Audio": string;
+  "format": number;
+  "user": any;  
+}
--- MongoBooks2/JavaScript/Node/AngularCLI/my-project/src/app/books/books.component.ts
+++ MongoBooks2/JavaScript/Node/AngularCLI/my-project/src/app/books/books.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from './Book';
 import IBook = books.IBook;
+import { BookData } from './BookData';
+import { MockBooksSet } from './MockBooks';
 
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css']
 })
+
 export class BooksComponent implements OnInit {
 
   book1: IBook = new Book("5a6b69dc6488960e6808bad0",
@@ -20,10 +23,12 @@ export class BooksComponent implements OnInit {
    "Spanish",
    "https://images-eu.ssl-images-amazon.com/images/I/51XOjayPg3L._SY291_BO1,204,203,200_QL40_.jpg",
    ["Magic Realism"],
-   1);
+    1);
 
   constructor() { }
   author = 'Joseph Roth';
+
+  BooksSet = MockBooksSet;
   ngOnInit() {
   }
 
