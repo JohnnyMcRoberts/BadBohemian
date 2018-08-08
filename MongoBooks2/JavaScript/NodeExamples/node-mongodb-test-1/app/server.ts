7c678e867cd4e5fbe2bb0fa10d2470488b900734
Johnathan McRoberts <jmcroberts@actify.com>
2018-08-08 17:14:53 +0100

    added app + some routes

--- MongoBooks2/JavaScript/NodeExamples/node-mongodb-test-1/app/server.ts
+++ MongoBooks2/JavaScript/NodeExamples/node-mongodb-test-1/app/server.ts
@@ -1,21 +1,7 @@
 /* app/server.ts */
+import app from "./app";
+const PORT = 3000;
 
-// Import everything from express and assign it to the express variable
-import * as express from 'express';
-
-// Import WelcomeController from controllers entry point
-import {WelcomeController} from './controllers';
-
-// Create a new express application instance
-const app: express.Application = express();
-// The port the express app will listen on
-const port: number = 3000;
-
-// Mount the WelcomeController at the /welcome route
-app.use('/welcome', WelcomeController);
-
-// Serve the application at the given port
-app.listen(port, () => {
-  // Success callback
-  console.log(`Listening at http://localhost:${port}/`);
-});
+app.listen(PORT, () => {
+  console.log('Express server listening on port ' + PORT);
+})
