7fbf53d7ff467944e6fbc2835f84c5ceed222f32
Johnathan McRoberts <jmcroberts@actify.com>
2018-09-03 18:31:37 +0100

    Add the extra TypeScript classes so now have a working express app

---
+++ MongoBooks2/JavaScript/Books2/ExpressBooks/BooksExpressApp/routes/allBooksHack.ts
@@ -0,0 +1,27 @@
+﻿/*
+ * GET all books listing.
+ */
+import express = require('express');
+const router = express.Router();
+
+var hostname: string;
+router.get('/', (req: express.Request, res: express.Response) => {
+
+  console.log('router.get allBooksHack / ');
+  hostname = req.hostname;
+  res.render(
+    'allBooksHack',
+    {
+      title: 'Express all books hack',
+      extra: 'some extra content -poor',
+      user: hostname,
+      books:
+      [
+          { author: "Georges Simenon", title: "Maigret is Afraid" },
+          { author: "Richard House", title: "The Kills" },
+          { author: "Ann Quin", title: "The Unmapped Country" }
+      ]
+    });
+});
+
+export default router;
---
+++ MongoBooks2/JavaScript/Books2/ExpressBooks/BooksExpressApp/routes/index.ts
@@ -0,0 +1,12 @@
+﻿/*
+ * GET home page.
+ */
+import express = require('express');
+const router = express.Router();
+
+router.get('/', (req: express.Request, res: express.Response) => {
+  console.log('router.get / ');
+    res.render('index', { title: 'Express (Not the jungle or Terrordome)' });
+});
+
+export default router;
