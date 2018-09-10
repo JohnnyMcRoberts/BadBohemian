eff5c6e903fe5db17abc23c302cea0bfdb324a7c
Johnathan McRoberts <jmcroberts@actify.com>
2018-09-10 17:48:02 +0100

    All the data for a monthly book report

---
+++ MongoBooks2/JavaScript/Books2/ExpressBooks/BooksExpressApp/routes/BooksTotal.ts
@@ -0,0 +1,64 @@
+﻿import { IBookRead, Format } from "./book";
+
+export class BooksTotal {
+  public totalDays: number;
+  public totalBooks: number;
+  public totalPages: number;
+
+  public totalBookFormat: number;
+  public totalComicFormat: number;
+  public totalAudioFormat: number;
+
+  public percentageInEnglish: number;
+  public percentageInTranslation: number;
+
+  public pageRate: number;
+  public daysPerBook: number;
+  public pagesPerBook: number;
+  public booksPerYear: number;
+
+  constructor(days: number, books: IBookRead[]) {
+    this.totalDays = days;
+    this.totalBooks = books.length;
+
+    // Default the totals to zero.
+    let totalInEnglish: number = 0;
+    let totalInTranslation: number = 0;
+    this.totalPages = 0;
+    this.totalBookFormat = 0;
+    this.totalComicFormat = 0;
+    this.totalAudioFormat = 0;
+
+    // Get the totals from the books list.
+    for (let book of books) {
+      this.totalPages += book.pages;
+
+      if (book.originalLanguage !== "English") {
+        totalInTranslation++;
+      }
+      else {
+        totalInEnglish++;
+      }
+
+      if (book.format === Format.Book) {
+        this.totalBookFormat++;
+      }
+      else if (book.format === Format.Audio) {
+        this.totalAudioFormat++;
+      }
+      else if (book.format === Format.Comic) {
+        this.totalComicFormat++;
+      }
+    }
+
+    // Calculate the percentages and rates.
+    this.percentageInEnglish = (totalInEnglish * 100.0) / this.totalBooks;
+    this.percentageInTranslation = (totalInTranslation * 100.0) / this.totalBooks;
+
+    // Calculate the percentages and rates.
+    this.pageRate = this.totalPages / this.totalDays;
+    this.daysPerBook = this.totalDays / this.totalBooks;
+    this.pagesPerBook = this.totalPages / this.totalBooks;
+    this.booksPerYear = 365.25 / this.daysPerBook;
+  }
+}
--- MongoBooks2/JavaScript/Books2/ExpressBooks/BooksExpressApp/routes/allBooksByMonthMongo.ts
+++ MongoBooks2/JavaScript/Books2/ExpressBooks/BooksExpressApp/routes/allBooksByMonthMongo.ts
@@ -9,7 +9,9 @@ import Collections = require('typescript-collections');
 import Book from './book'
 import { IBook } from "./book";
 import { BookRead } from './BookRead'
-import { MonthDate, MonthlyBooksRead } from './MonthlyBooksRead'
+import { MonthDate } from './MonthDate'
+import { BooksTotal } from './BooksTotal'
+import { MonthlyBooksRead } from './MonthlyBooksRead'
 
 
 const router = express.Router();
@@ -17,44 +19,48 @@ const router = express.Router();
 var hostname: string;
 var listBook: IBook;
 var listBookItems: IBook[];
-var listBooksRead: BookRead[];
 
 
 function getMonthlyBooksRead(books: IBook[]): MonthlyBooksRead[]
 {
-  var dict = new Collections.Dictionary<string, MonthlyBooksRead>();
+  const dict = new Collections.Dictionary<MonthDate, MonthlyBooksRead>();
   for (let book of books) {
     const month = new MonthDate(book.date);
-    var monthName = month.year.toString() + '/' + month.month.toString();
 
-    if (dict.containsKey(monthName)) {
-      const monthSet = dict.getValue(monthName);
+    if (dict.containsKey(month))
+    {
+      const monthSet = dict.getValue(month);
       monthSet.listBooksRead.push(new BookRead(book));
-      dict.setValue(monthName, monthSet);
-
-      console.log('adding book ' + book.title + ' to existing month ' + month); // "4", "5", "6"
+      dict.setValue(month, monthSet);
     }
     else
     {
       const monthOfBooks = new MonthlyBooksRead(book.date);
       monthOfBooks.listBooksRead.push(new BookRead(book));
-      dict.setValue(monthName, monthOfBooks);
-      console.log('adding book ' + book.title + ' to new month ' + month); // "4", "5", "6"
+      dict.setValue(month, monthOfBooks);
     }
-
   }
 
   return dict.values();
 }
 
+function randomIntFromInterval(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getDifferenceInDays(start: Date, end: Date): number
+{
+  let diff: number = Math.abs(start.getTime() - end.getTime());
+  let diffDays: number = Math.ceil(diff / (1000 * 3600 * 24));
+  return diffDays;
+}
 
 router.get('/', (req: express.Request, res: express.Response) => {
 
-  console.log('router.get allBooksByMonthMongo / ');
   hostname = req.hostname;
+  console.log('router.get allBooksByMonthMongo for host ' + hostname + ' / ');
 
   listBookItems = new Array<IBook>();
-  listBooksRead = new Array<BookRead>();
 
   let books = Book.find((err: any, books: mongoose.Document[]) => {
     if (err)
@@ -74,38 +80,43 @@ router.get('/', (req: express.Request, res: express.Response) => {
     }
     else
     {
-      console.log('successfully got the books -> ', books.toString().slice(0, 50));
+      console.log('successfully got the books');
+      //console.log('successfully got the books -> ', books.toString().slice(0, 50));
 
       let i: number = 0;
       for (let book in books) {
         if (books.hasOwnProperty(book))
         {
           listBook = books[i] as IBook;
-          //console.log('successfully got the book ' + i + ' -> ', listBook);
           i++;
 
-          var bookRead = new BookRead(listBook);
           listBookItems.push(listBook);
-
-          if (i > 5)
-            continue;
-
-          listBooksRead.push(bookRead);
         }
       }
 
-      var bookMonths = getMonthlyBooksRead(listBookItems);
-      var fourthMonth = bookMonths[4];
+      let bookMonths = getMonthlyBooksRead(listBookItems);
+
+      let monthIndex = randomIntFromInterval(0, bookMonths.length);
+
+      let randomMonth = bookMonths[monthIndex];
+      randomMonth.updateTotals();
+
+      const totalDays: number = getDifferenceInDays(listBookItems[0].date, listBookItems[listBookItems.length - 1].date);
+
+      const overallTotals: BooksTotal = new BooksTotal(totalDays, listBookItems);
       
       res.render(
         'allBooksByMonthMongo',
         {
-          title: 'Express got a subset of books from DB',
-          books: fourthMonth.listBooksRead
+          title: 'Express got a subset of books from DB for ' + randomMonth.monthDate + ' it has ' + randomMonth.monthDate.daysInMonth() + ' days. Overall ' + totalDays + ' Days' ,
+          monthDate: randomMonth.monthDate,
+          monthTotals: randomMonth.monthTotals,
+          monthlyBreakdownTotals: randomMonth.breakdownTotals,
+          overallTotals: overallTotals,
+          books: randomMonth.listBooksRead
         });
     }
   });
-
 
 });
 
