0dc1f0ef086223b851b09e2aeaa47843edb52eaa
Johnathan McRoberts <jmcroberts@actify.com>
2018-09-19 11:57:59 +0100

    more

--- MongoBooks2/JavaScript/Books2/AngularBooks/BooksAngular5App/ClientApp/app/services/allNames/allNames.service.ts
+++ MongoBooks2/JavaScript/Books2/AngularBooks/BooksAngular5App/ClientApp/app/services/allNames/allNames.service.ts
@@ -10,7 +10,12 @@ export interface Author {
 export class AuthorService {
   constructor(private http: HttpClient) { }
 
+  private uri: string = 'http://localhost:3000/authors';
+
   getAllAuthors(): Observable<Author[]> {
-    return this.http.get<Author[]>('http://localhost:3000/authors');
+    console.log('-> getting authors from : ' + this.uri);
+    let result = this.http.get<Author[]>(this.uri, { responseType: 'json' });
+    console.log('-> done getting authors from : ' + this.uri);
+    return result;
   }
 }
