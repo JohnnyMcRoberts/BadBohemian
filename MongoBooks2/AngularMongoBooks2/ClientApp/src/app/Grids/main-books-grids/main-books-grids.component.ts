566116ba5f30aa771977e5a1955c80e13e5ecd00
Johnny McRoberts <johnathanmcroberts@gmail.com>
2019-02-16 17:54:51 +0000

    a new start

---
+++ MongoBooks2/AngularMongoBooks2/ClientApp/src/app/Grids/main-books-grids/main-books-grids.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-main-books-grids',
+    templateUrl: './main-books-grids.component.html',
+    styleUrls: ['./main-books-grids.component.scss']
+})
+/** MainBooksGrids component*/
+export class MainBooksGridsComponent {
+    /** MainBooksGrids ctor */
+    constructor() {
+
+    }
+}
