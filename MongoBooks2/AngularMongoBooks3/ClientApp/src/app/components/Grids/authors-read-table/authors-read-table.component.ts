b6f6c0f4c920acb75843d4da07e0d0c1b9df0903
Johnny McRoberts <johnathanmcroberts@gmail.com>
2019-02-23 18:18:25 +0000

    Using the providers properly + fixed some more layout

---
+++ MongoBooks2/AngularMongoBooks3/ClientApp/src/app/components/Grids/authors-read-table/authors-read-table.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-authors-read-table',
+    templateUrl: './authors-read-table.component.html',
+    styleUrls: ['./authors-read-table.component.scss']
+})
+/** AuthorsReadTable component*/
+export class AuthorsReadTableComponent {
+    /** AuthorsReadTable ctor */
+    constructor() {
+
+    }
+}
