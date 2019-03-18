e9616243846c3abe7567ebf3991de931fd46cd8d
Johnathan McRoberts <jmcroberts@actify.com>
2019-03-18 17:46:33 +0000

    Completed the tables tabs

---
+++ MongoBooks2/AngularMongoBooks3/ClientApp/src/app/Models/monthlyTally.ts
@@ -0,0 +1,42 @@
+import { Book } from './book'
+
+export interface IMonthlyTally
+{
+  monthDate: Date;
+  name: string;
+  daysInTheMonth: number;
+  totalBooks: number;
+  totalPagesRead: number;
+  totalBookFormat: number;
+  totalComicFormat: number;
+  totalAudioFormat: number;
+  percentageInEnglish: number;
+  percentageInTranslation: number;
+  pageRate: number;
+  daysPerBook: number;
+  pagesPerBook: number;
+  booksPerYear: number;
+  books: Book[];
+};
+
+export class MonthlyTally implements IMonthlyTally
+{
+  constructor(
+    public monthDate: Date = new Date(),
+    public name: string = "",
+    public daysInTheMonth: number = 0,
+    public totalBooks: number = 0,
+    public totalPagesRead: number = 0,
+    public totalBookFormat: number = 0,
+    public totalComicFormat: number = 0,
+    public totalAudioFormat: number = 0,
+    public percentageInEnglish: number = 0,
+    public percentageInTranslation: number = 0,
+    public pageRate: number = 0,
+    public daysPerBook: number = 0,
+    public pagesPerBook: number = 0,
+    public booksPerYear: number = 0,
+    public books: Book[] = new Array<Book>())
+  {
+  }
+}
---
+++ MongoBooks2/AngularMongoBooks3/Controllers/DataClasses/TagBooks.cs
@@ -0,0 +1,45 @@
+﻿namespace AngularMongoBooks3.Controllers.DataClasses
+{
+    using BooksCore.Books;
+
+    public class TagBooks
+    {
+        /// <summary>
+        /// Gets the tag.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets the total number of pages this tag applies to.
+        /// </summary>
+        public int TotalPages { get; set; }
+
+        /// <summary>
+        /// Gets the total number of books this tag applies to.
+        /// </summary>
+        public int TotalBooks { get; set; }
+
+        /// <summary>
+        /// Gets or sets the set of books with this tag.
+        /// </summary>
+        public Book[] Books { get; set; }
+
+        public TagBooks()
+        {
+
+        }
+
+        public TagBooks(BookTag bookTag)
+        {
+            Name = bookTag.Tag;
+            TotalPages = bookTag.TotalPages;
+            TotalBooks = bookTag.TotalBooksReadBy;
+
+            Books = new Book[bookTag.BooksWithTag.Count];
+            for (int i = 0; i < bookTag.BooksWithTag.Count; i++)
+            {
+                Books[i] = new Book(bookTag.BooksWithTag[i]);
+            }
+        }
+    }
+}
