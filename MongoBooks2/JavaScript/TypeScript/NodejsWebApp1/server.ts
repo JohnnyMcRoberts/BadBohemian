1639414aa91f0f479882ce99307add931477cbd3
Johnathan McRoberts <jmcroberts@actify.com>
2019-08-28 10:42:12 +0100

    delete all the old projects now they are generating zillions of warnings

--- MongoBooks2/JavaScript/TypeScript/NodejsWebApp1/server.ts
+++
@@ -1,8 +0,0 @@
-﻿import http = require('http');
-var port = process.env.port || 1337
-http.createServer(function (req, res) {
-
-  console.log("Got a request!")
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
